89f5ae0e0e06ce2bc69f0d75fb4fc817bb5166b0
Varun Adhithya S <119162974+varun82004@users.noreply.github.com>
2025-07-03 17:40:28 +0530

    Create userModel.js

---
+++ backend/models/userModel.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    role:{
+        type:String,
+        required:[true,'role is required'],
+        enum:['admin','organization','user','hospital','donar']
+    },
+    name:{
+        type:String,
+        required: function() {
+            if (this.role==='user' || this.role==='admin'){
+                return true;
+            }
+            return false;
+        }
+    },
+    organizationName:{
+        type:String,
+        required:function(){
+            if(this.role==='organization'){
+                return true;
+            }
+            return false;
+        }
+    },
+    hospitalName:{
+        type:String,
+        required:function(){
+            if(this.role==='hospital'){
+                return true;
+            }
+            return false;
+        }
+    },
+    email:{
+        type:String,
+        required:[true,'email is required'],
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:[true,'password is required']
+    },
+    website:{
+        type:String,
+    },
+    address:{
+        type:String,
+        required:[true,'address is required']
+    },
+    phone:{
+        type:String,
+        required:[true,'phone is required']
+    }
+},{timestamps:true});
+
+export default mongoose.model("User", userSchema);
